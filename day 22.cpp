5425832c8b62ae9291001fcfaf1d1de9be019e61
sachin joseph <sachinjoseph054@gmail.com>
2020-12-22 23:09:50 +0530

    Add files via upload

---
+++ day 22.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+          //Write your code here
+          if(root==NULL)
+                return 0;
+        else {
+            int ldepth=getHeight(root->left);
+            int rdepth=getHeight(root->right);
+            return max(ldepth,rdepth);
+        }
+          
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
+    
+       
