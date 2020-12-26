fd10669fb0ab158bdd045f8536ab08470a3fb450
sachin joseph <sachinjoseph054@gmail.com>
2020-12-26 20:03:30 +0530

    Add files via upload

---
+++ day 23.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+      //Write your code here
+  	//if(root==NULL)
+      //return;
+    queue<Node *>qu;
+    qu.push(root);
+    while (qu.empty()==false) {
+        Node *node=qu.front();
+        qu.pop();
+        cout<<node->data<<"\t";
+        
+        if(node->left!=NULL)    
+            qu.push(node->left);
+        if(node->right!=NULL)
+            qu.push(node->right);
+    }
+  
+	}
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
